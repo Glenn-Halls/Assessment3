378cdf056ecdbb895db7b9b89d50218f73d2d7a3
Glenn Halls <gjhalls@gmail.com>
2023-05-22 14:28:38 +1000

    Fix: PrintBookingDetails documentation

--- src/main/java/PrintBookingDetails.java
+++ src/main/java/PrintBookingDetails.java
@@ -2,6 +2,9 @@ import java.text.DecimalFormat;
 import java.text.NumberFormat;
 import java.time.format.DateTimeFormatter;
 
+/**
+ * Displays details of the finalised booking to the user.
+ */
 public class PrintBookingDetails {
 
     // Class values
@@ -11,16 +14,31 @@ public class PrintBookingDetails {
     final int COLUMN_WIDTH = 20;
     final double INSURANCE = 0.05;
 
-
+    /**
+     * Class constructor will receive the car booking and customer object, then
+     * will present all relevant details of the booking to the user, in a
+     * formatted and tabulated display.
+     * @param carBooking Represents the booking.
+     * @param customer Represents the customer.
+     */
     public PrintBookingDetails(CarBooking carBooking, Customer customer) {
-        // Helper method to format number into currency.
+        // Helper method to format number into currency display.
         NumberFormat nf = NumberFormat.getCurrencyInstance();
 
         // Helper method to format date.
         DateTimeFormatter dtf = DateTimeFormatter.ofPattern("dd MMMM yyyy");
 
-        // Helper for formatting decimals
+        // Helper for formatting decimals.
         DecimalFormat df = new DecimalFormat("#.##");
+
+        // Daily cost of car hire.
+        double rate = carBooking.getNewRate();
+
+        // Indicates weather or not car is premium.
+        boolean premiumCar = (carBooking.car instanceof PremiumCar);
+
+        // Represents the advertised cost of car hire for a premium car.
+        double price = (premiumCar) ? rate / (1 + INSURANCE) : rate;
 
         // Allocation of car and booking values into String variables
         String customerName = customer.getCustomerName();
@@ -30,20 +48,13 @@ public class PrintBookingDetails {
         String startDate = carBooking.getStartDate().format(dtf);
         String endDate = carBooking.getEndDate().format(dtf);
         String numDays = String.valueOf(carBooking.getTotalDays());
-        double rate = carBooking.getNewRate();
         String ratePerDay = nf.format(rate);
-        boolean premiumCar = (carBooking.car instanceof PremiumCar);
         String isCarPremium = (premiumCar) ? "Yes" : "No";
-        double price = (premiumCar) ? rate / (1 + INSURANCE) : rate;
         String advertisedPrice = nf.format(price);
         String insurance = ((premiumCar) ? df.format(INSURANCE*100) : 0) + "%";
         String totalCost = nf.format(carBooking.getCost());
 
-
-
-
-
-        // Booking details banner
+        // Booking details banner.
         printBanner(SCREEN_WIDTH, '-');
         pageBanner('*');
         centreText("Thank you for your booking!");
@@ -54,14 +65,14 @@ public class PrintBookingDetails {
         pageBanner('~');
         System.out.println();
 
-        // Customer information
+        // Customer information.
         printLine("Name:", customerName);
         printLine("Email:", customerEmail);
         printLine("Address:", customerAddress);
         pageBanner('.');
         System.out.println();
 
-        // Booking information
+        // Booking information.
         centreText("Booking information");
         System.out.println();
         printLine("Car", carName);
@@ -69,6 +80,7 @@ public class PrintBookingDetails {
         printLine("Last day of hire", endDate);
         printLine("Number of days", numDays);
         printLine("Premium car?", isCarPremium);
+        // Additional lines will be displayed only if selected car is "Premium".
         if (premiumCar) {
             printLine("Advertised price", advertisedPrice);
             printLine("Insurance rate", insurance);
@@ -78,8 +90,6 @@ public class PrintBookingDetails {
         centreText("Total cost: " + totalCost);
         pageBanner('-');
         printBanner(SCREEN_WIDTH, '_');
-
-
     }
 
     // Helper method to print banner decoration.
@@ -103,6 +113,7 @@ public class PrintBookingDetails {
         printBanner(PAGE_WIDTH, decoration);
     }
 
+    // Helper method to centre text in middle of display area.
     private void centreText(String message) {
         if (message.length() >= PAGE_WIDTH) {
             System.out.println();
@@ -114,6 +125,10 @@ public class PrintBookingDetails {
         }
     }
 
+    /*
+     * Helper method will print string variables in the left and right columns
+     * of the display as dictated by the margin, page width and column widths.
+     */
     private void printLine(String leftColumn, String rightColumn) {
         printWhitespace(MARGIN);
         System.out.print(leftColumn);
@@ -121,5 +136,4 @@ public class PrintBookingDetails {
         System.out.print(rightColumn);
         System.out.println();
     }
-
 }
